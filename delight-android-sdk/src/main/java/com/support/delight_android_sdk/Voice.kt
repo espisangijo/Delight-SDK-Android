ea0bd11838d5dccb1fc27a91f4dfac3c3fff7ecd
Jacky <jackyangara@gmail.com>
2021-08-24 11:22:50 +0800

    update java version

--- delight-android-sdk/src/main/java/com/support/delight_android_sdk/Voice.kt
+++ delight-android-sdk/src/main/java/com/support/delight_android_sdk/Voice.kt
@@ -1,4 +1,10 @@
 package com.support.delight_android_sdk
 
+import android.util.Log
+
 class Voice {
+    private val TAG = "VOICE"
+    fun VoiceFunction() {
+        Log.d(TAG, "VoiceFunction")
+    }
 }
