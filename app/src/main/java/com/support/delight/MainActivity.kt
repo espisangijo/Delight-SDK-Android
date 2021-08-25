a130962ae1ea8367b55966a25d8138209f64b735
Jacky <jackyangara@gmail.com>
2021-08-25 14:40:46 +0800

    add text input and button

--- app/src/main/java/com/support/delight/MainActivity.kt
+++ app/src/main/java/com/support/delight/MainActivity.kt
@@ -2,13 +2,18 @@ package com.support.delight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.support.delight_android_sdk.Voice
+import com.support.delight_android_sdk.ChatFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val chatFragment = ChatFragment();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.accelerate, chatFragment)
     }
 
 }
--- delight-android-sdk/src/main/java/com/support/delight_android_sdk/ChatFragment.kt
+++ delight-android-sdk/src/main/java/com/support/delight_android_sdk/ChatFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.support.delight_android_sdk.model.DelightResponse
 import com.support.delight_android_sdk.repository.Repository
 import org.w3c.dom.Text
 
@@ -17,7 +21,6 @@ import org.w3c.dom.Text
 class ChatFragment : BottomSheetDialogFragment() {
 
     private lateinit var viewModel: DelightViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,27 +35,30 @@ class ChatFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view : View = inflater.inflate(R.layout.fragment_chat, container, false)
-        val textView = view.findViewById<TextView>(R.id.response)
+        val textView = view.findViewById<TextView>(R.id.text_view)
+        val buttonView = view.findViewById<Button>(R.id.send_message)
+        val editTextView = view.findViewById<EditText>(R.id.text_input_edittext)
         textView.setOnClickListener {
-            activity?.let {
-                Toast.makeText(activity, "text", Toast.LENGTH_LONG).show()
-            }
-            Log.d("ChatFragment", "Clicked")
+            changeText((1..6).random().toString())
+            Log.d("ChatFragment", "TextView Clicked")
         }
+
+        buttonView.setOnClickListener {
+            changeText(editTextView.text.toString())
+            Log.d("ChatFragment","Button Clicked")
+        }
+
+        viewModel.myResponse.observe(this, object : Observer<DelightResponse?> {
+            override fun onChanged(t: DelightResponse?) {
+                changeText(t?.text)
+                Log.d("ChatFragment", "Change")
+            }
+        })
+
         return view
     }
 
-    fun changeText(text: String) {
-        val myText : TextView? = activity?.findViewById<TextView>(R.id.response)
-        if (myText != null) {
-            myText.setText(text?: "")
-        }
+    fun changeText(text: String?) {
+        activity?.findViewById<TextView>(R.id.text_view)?.text = text ?: ""
     }
-//    fun newInstance(): Fragment() {
-//        val args = Bundle()
-//
-//        val fragment = ()
-//        fragment.arguments = args
-//        return fragment
-//    }
 }
